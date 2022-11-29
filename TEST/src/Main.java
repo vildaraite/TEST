a9c306b24d67181f16dea401853f622260b1979a
vildaraite <119286337+vildaraite@users.noreply.github.com>
2022-11-29 18:52:27 +0200

    Update Main.java

--- TEST/src/Main.java
+++ TEST/src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        
+        String text = "Hello world, I'm back"
+            System.out.printl(text);
     }
 }
